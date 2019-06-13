a61aa118c159308abfd9ce44dac0ac60d3d5fe99
Danny Lin <danny@kdrag0n.dev>
2019-06-12 22:06:22 -0700

    airootfs: Add pre-ranked pacman mirror list
    These mirrors were ranked at a location near Seattle, Washington. Only
    mirrors located in the United States were considered.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -21,7 +21,7 @@ sed -i 's/#\(HandleSuspendKey=\)suspend/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
 
-systemctl enable pacman-init.service choose-mirror.service
+systemctl enable pacman-init.service
 systemctl enable NetworkManager.service
 systemctl set-default multi-user.target
 
