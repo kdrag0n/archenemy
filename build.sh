11a8f0b69b23133bd98de406348ba6aa867a911d
Danny Lin <danny@kdrag0n.dev>
2019-06-12 20:24:06 -0700

    Apply ArchEnemy Linux branding
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 set -e -u
 
-iso_name=archlinux
+iso_name=archenemy
 iso_label="ARCH_$(date +%Y%m)"
 iso_publisher="Arch Linux <http://www.archlinux.org>"
-iso_application="Arch Linux Live/Rescue CD"
+iso_application="ArchEnemy Linux Live/Rescue CD"
 iso_version=$(date +%Y.%m.%d)
 install_dir=arch
 work_dir=work
